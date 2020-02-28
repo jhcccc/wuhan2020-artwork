65fd036620542bbec39468124bd4a501f7a1b053
Jiahao <jahowchen@outlook.com>
2020-02-28 14:16:17 -0500

    Update photos.js

--- src/photos.js
+++ src/photos.js
@@ -1,8 +1,8 @@
 let baseURL = "https://github.com/jhcccc/wuhan2020-artwork/blob/master/img/"
 let imgEntension = ".jpg?raw=true";
-let imgCount = 12;
+let imgCount = 14;
 let photos = [];
-let widthHeightList = [[4,3],[3,5],[3,5],[3,5],[1,1],[5,3],[5,3],[3,5],[1,1],[1,1],[3,5],[3,5],[3,5]];
+let widthHeightList = [[4,3],[3,5],[3,5],[3,5],[1,1],[5,3],[5,3],[3,5],[1,1],[1,1],[3,5],[3,5],[3,5],[5,3],[3,4]];
 for(let i=0;i<imgCount;i++){
   photos.push(
   {
